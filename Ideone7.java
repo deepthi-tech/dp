86c3c654afb4e3d3dc2dd83298cb9de328dad345
deepthi-tech <52168031+deepthi-tech@users.noreply.github.com>
2019-06-25 00:27:00 +0530

    Create Ideone7.java

---
+++ Ideone7.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+              
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+int sum=0;
+                for(int i=0;i<=a;i++)
+                 { 
+                sum=sum+i;       
+                  }
+                System.out.println(sum);
+		
+	}
+}
