ce4b0c15eec7ae64a1207a5c683a5d528482cbe7
deepthi-tech <52168031+deepthi-tech@users.noreply.github.com>
2019-06-25 01:01:33 +0530

    Create Ideone9.java

---
+++ Ideone9.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+              
+		Scanner scan = new Scanner(System.in);
+                int a=scan.nextInt();
+                 int b=scan.nextInt();
+                 int c=scan.nextInt();
+if(a>b)
+{
+if(a>c)
+{
+System.out.println(a +"is greater");
+}
+}
+else if(b>c)
+{
+System.out.println(b +"is greater");
+}
+else
+{
+System.out.println(c +"is greater");
+}
+                
+	}
+}
