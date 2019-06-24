47faa119a37fff86131786881b72db2036f210e7
deepthi-tech <52168031+deepthi-tech@users.noreply.github.com>
2019-06-25 00:22:08 +0530

    Create Ideone6.java

---
+++ Ideone6.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone6
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+              
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+                for(int i=0;i<a;i++)
+                 {
+                 System.out.println("Hello");
+                  }
+                
+		
+	}
+}
