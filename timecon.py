097d3afa51f701d904a58f573fa52218f8da96c9
deepthi-tech <52168031+deepthi-tech@users.noreply.github.com>
2019-06-29 19:37:44 +0530

    Update timecon.py

--- timecon.py
+++ timecon.py
@@ -2,11 +2,10 @@ time=int(input())
 mins=0
 hours=time//60
 if(time>60):
-    mins=int(((time/60)-hours)*100)
-if(mins>60):
-    time=59
-else:
-    time=mins
+    mins=(time/60)-hours
+    time=int(mins)
+if(mins>0.60):
+    time=int(mins*60)
 print(hours,time)
 
 
