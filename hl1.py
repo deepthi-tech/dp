a1fd56c131937a90c5059a5ff79e6d6329fbbea5
deepthi-tech <52168031+deepthi-tech@users.noreply.github.com>
2019-07-03 00:12:33 +0530

    Create hl1.py

---
+++ hl1.py
@@ -0,0 +1,15 @@
+a=[]
+x=input().split()
+for i in x:
+    a.append(int(i))
+b=len(a)
+j=0
+k=0
+t=0
+for j in range(0,b):
+    for k in range(j+1,b):
+        if(a[j]>a[k]):
+            t=a[j]
+            a[j]=a[k]
+            a[k]=t
+print(a[0],a[b-1])
